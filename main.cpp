a9a1f682dd389744c1acec743bf2efef9d328d90
Babacar Fall <Fallbaba136@gmail.com>
2025-10-14 00:45:42 +0200

    pratical work done

--- main.cpp
+++ main.cpp
@@ -1,119 +1,136 @@
 #include <iostream>
-#include <string>
 using namespace std;
-// Arbre binaire de recherche
-//creation d'un arbre binaire de recherche
-struct noeud
-{
+
+
+// Structure d’un nœud d’ABR
+
+struct noeud {
     int cle;
-    noeud* gauche;
-    noeud* droite;
-    noeud* pere;
+    noeud *gauche;
+    noeud *droite;
+    noeud *pere;
 };
-void createUser(noeud** racine)
-{
-    noeud* newNode = new noeud;
-    newNode->pere = NULL;
-    newNode->gauche = NULL;
-    newNode->droite = NULL;
-    cout << "Entrer une valeur : ";
-    cin >> newNode->cle;
-    *racine = newNode;
-    char reponse;
-    cout << "Voulez vous d'un fils gauche O/N: ";
-    cin >> reponse;
-    if (reponse == 'o' || reponse == 'O' )
-    {
-        createUser(&(*racine)->gauche);
-        (*racine)->gauche->pere = *racine;
+
+
+// Insertion itérative dans un ABR
+
+void insertNoeudIte(noeud **racine, noeud *insert) {
+    noeud *tmp = *racine;
+    noeud *father = NULL;
+
+    while (tmp != NULL) {
+        father = tmp;
+        if (insert->cle < tmp->cle)
+            tmp = tmp->gauche;
+        else
+            tmp = tmp->droite;
     }
-    cout << "Voulez vous d'un fils droite O/N: ";
-    cin >> reponse;
-    if (reponse == 'o' || reponse == 'O')
-    {
-        createUser(&(*racine)->droite);
-        (*racine)->droite->pere = *racine;
-    }
-}
-void AfficheArbre(noeud* racine)
-{
-    if (racine == NULL) return;
-    
-    AfficheArbre(racine->gauche);
-    cout << racine->cle << " -> ";
-    AfficheArbre(racine->droite);
-    cout << endl;
-}
-//Requetes
-noeud* noeudValue(noeud* racine, int cleRecherche)
-{
-    if(racine == NULL) return NULL;
-    if (racine->cle == cleRecherche)
-    {
-        return racine;
-    }
-    if (racine->cle > cleRecherche)
-    {
-        return  noeudValue(racine->gauche, cleRecherche);
-    }
-    return noeudValue(racine->droite, cleRecherche);
-}
-noeud* rechercheCleMin(noeud* racine)
-{
-    if (racine == NULL) return NULL;
-    if (racine->gauche == NULL)
-    {
-        return racine;
-    }
-    return rechercheCleMin(racine->gauche);
-    
-}
-noeud* rechercheCleMax(noeud* racine)
-{
-    if (racine == NULL) return NULL;
-    if (racine->droite == NULL)
-    {
-        return racine;
-    }
-    return rechercheCleMax(racine->droite);
-}
-// le predecesseur est le maximum de sa sous arbre gauche
-// si le noeud n'a pas de sous arbre gauche son predecesseur est le premier ancetre dont son sous arbre droite contient le noeud
-noeud* recherchePre(noeud* racine)
-{
-    if (racine == NULL)return  NULL;
-    if (racine->gauche != NULL)
-    {
-        return rechercheCleMax(racine->gauche);
-        noeud* father = racine->pere;
-        while (father != NULL && father->gauche == racine)
-        {
-            racine = father;
-            father = father->pere;
-        }
-        return father;
-    }
-    return racine;
-    
+
+    insert->pere = father;
+    insert->gauche = NULL;
+    insert->droite = NULL;
+
+    if (father == NULL)
+        *racine = insert; // arbre vide → racine
+    else if (insert->cle < father->cle)
+        father->gauche = insert;
+    else
+        father->droite = insert;
 }
 
-int main(int argc, const char * argv[])
-{
-    int cle;
-    cout << "Entrer le cle recherche : ";
-    cin >> cle;
-    noeud* newNode = NULL;
-    createUser(&newNode);
-    AfficheArbre(newNode);
-    noeud* recherche = noeudValue(newNode, cle);
-    cout << " L'adresse du cle est : " << recherche << endl;
-    cout << endl;
-    //noeud* result1 = rechercheCleMin(newNode);
-    //noeud* result2 = rechercheCleMax(newNode);
-    //cout << " le min de l'arbre est : " << result1->cle << " et le max de l'arbre est : " << result2->cle << endl;
-    
-    
-    
-    
+// Construire un ABR à partir d’un tableau
+void createABRFromArray(noeud **racine, int tab[], int taille) {
+    for (int i = 0; i < taille; i++) {
+        noeud *n = new noeud;
+        n->cle = tab[i];
+        n->gauche = n->droite = n->pere = NULL;
+        insertNoeudIte(racine, n);
+    }
+}
+
+// Parcours infixe (affichage trié)
+void AfficheArbre(noeud *racine) {
+    if (racine == NULL) return;
+    AfficheArbre(racine->gauche);
+    cout << racine->cle << " ";
+    AfficheArbre(racine->droite);
+}
+
+// Transformer un ABR → tableau trié
+void arbreToArray(noeud *racine, int tab[], int *index) {
+    if (racine == NULL) return;
+    arbreToArray(racine->gauche, tab, index);
+    tab[*index] = racine->cle;
+    (*index)++;
+    arbreToArray(racine->droite, tab, index);
+}
+
+void createArrayFromABR(noeud *racine, int tab[], int taille) {
+    int index = 0;
+    arbreToArray(racine, tab, &index);
+}
+
+// Tri d’un tableau avec un ABR
+void triAvecABR(int tab[], int taille) {
+    noeud *racine = NULL;
+    createABRFromArray(&racine, tab, taille);
+    createArrayFromABR(racine, tab, taille);
+}
+
+
+// Créer un ABR équilibré à partir d’un tableau trié
+
+noeud* createABREquilibre(int tab[], int debut, int fin, noeud *pere = NULL) {
+    if (debut > fin) return NULL;
+
+    int milieu = (debut + fin) / 2;
+    noeud *n = new noeud;
+    n->cle = tab[milieu];
+    n->pere = pere;
+    n->gauche = createABREquilibre(tab, debut, milieu - 1, n);
+    n->droite = createABREquilibre(tab, milieu + 1, fin, n);
+
+    return n;
+}
+
+
+int main() {
+    int tab[] = {12, 7, 15, 9, 3, 13, 17};
+    int taille = 7;
+
+    cout << "Tableau original : ";
+    for (int i = 0; i < taille; i++) cout << tab[i] << " ";
+    cout << "\n";
+
+    //  Créer un ABR depuis le tableau
+    noeud *racine = NULL;
+    createABRFromArray(&racine, tab, taille);
+    cout << "Affichage infixe (ABR trié) : ";
+    AfficheArbre(racine);
+    cout << "\n";
+
+    //  Convertir ABR -> tableau trié
+    int sorted[20];
+    createArrayFromABR(racine, sorted, taille);
+    cout << "Tableau trié via ABR : ";
+    for (int i = 0; i < taille; i++) cout << sorted[i] << " ";
+    cout << "\n";
+
+    //  Trier directement un tableau via ABR
+    int tab2[] = {5, 1, 8, 2, 9};
+    int t2 = sizeof(tab2)/sizeof(tab2[0]);
+    triAvecABR(tab2, t2);
+    cout << "Tri avec ABR : ";
+    for (int i = 0; i < t2; i++) cout << tab2[i] << " ";
+    cout << "\n";
+
+    //  Créer un ABR équilibré
+    int tri[] = {1,2,3,4,5,6,7,8,9};
+    int t3 = sizeof(tri)/sizeof(tri[0]);
+    noeud *eq = createABREquilibre(tri, 0, t3 - 1);
+    cout << "ABR équilibré (affichage infixe) : ";
+    AfficheArbre(eq);
+    cout << "\n";
+
     return 0;
 }
