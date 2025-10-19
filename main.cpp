8c0076f22030443966d92c4be209accd73f3e9f2
Babacar Fall <Fallbaba136@gmail.com>
2025-10-19 04:04:32 +0200

    pratical work for ABR is done

--- main.cpp
+++ main.cpp
@@ -10,8 +10,88 @@ struct noeud {
     noeud *droite;
     noeud *pere;
 };
+//creation de l'arbre
+void createUser(noeud** racine)
+{
+    noeud* newNoeud = new noeud;
+    cout << "Entrer un nombre : ";
+    cin >> newNoeud->cle;
+    *racine = newNoeud;
+    char reponse;
+    cout << "Voulez vous un fils gauche (O/N) : ";
+    cin >> reponse;
+    if (reponse == 'o' || reponse == 'O')
+    {
+        createUser(&(*racine)->gauche);
+    }
+    else if (reponse == 'n' || reponse == 'N')
+    {
+        createUser(&(*racine)->droite);
+    }
+    else
+    {
+        (*racine)->gauche = NULL;
+        (*racine)->droite = NULL;
+    }
+}
+noeud* rechercheMin(noeud* racine)
+{
+    if(racine == NULL) return NULL;
+    if(racine->gauche == NULL) return racine;
+   
+    return rechercheMin(racine->gauche);
+}
+noeud* rechercheMax(noeud* racine)
+{
+    if(racine == NULL) return NULL;
+    if(racine->droite == NULL) return racine;
+   
+    return rechercheMax(racine->gauche);
+}
 
+noeud* rechercheSucc(noeud* racine)
+{
+    if(racine == NULL) return NULL;
+    if (racine->droite != NULL) return rechercheMin(racine->droite);
+    noeud* father = racine->pere;
+    while (father != NULL && racine == racine->pere)
+    {
+        racine = father;
+        father = father->pere;
+        return father;
+    }
+    return racine;
+}
+noeud* rechercheSuccRec(noeud* x)
+{
+    if(x->droite != NULL)
+        return rechercheMin(x->droite);
+    if(x->pere == NULL) return NULL;
+    if(x->pere->gauche == x) return x->pere;
+    return rechercheSuccRec(x->pere);
+}
 
+noeud* recherchePre(noeud* racine)
+{
+    if(racine == NULL) return NULL;
+    if (racine->droite != NULL) return rechercheMax(racine->gauche);
+    noeud* father = racine->pere;
+    while(father!=NULL && racine == racine->pere)
+    {
+        racine = father;
+        father = father->pere;
+        return father;
+    }
+    return racine;
+}
+noeud* recherchePreRec(noeud* y)
+{
+    if(y->gauche != NULL)
+        return rechercheMax(y->gauche);
+    if(y->pere == NULL) return y->pere;
+    if(y->pere->droite == y) return y->pere;
+    return recherchePreRec(y->pere);
+}
 // Insertion itérative dans un ABR
 
 void insertNoeudIte(noeud **racine, noeud *insert) {
@@ -37,6 +117,95 @@ void insertNoeudIte(noeud **racine, noeud *insert) {
     else
         father->droite = insert;
 }
+void insertRec(noeud* racine, noeud* insert)
+{
+    if (racine == NULL) insert = racine;
+    else
+    {
+        if(insert->cle < racine->cle)
+            insertRec(racine->gauche, insert);
+        else if(insert->cle > racine->cle)
+            insertRec(racine->droite, insert);
+    }
+    
+}
+bool supprimeIter(noeud** racine, noeud* courant)
+{
+    noeud* rattache;
+    noeud* asupprimer;
+    if(courant == NULL) return false;
+    if (courant->gauche == NULL || courant->droite == NULL)
+    {
+        asupprimer = courant;
+    }
+    else
+    {
+        asupprimer = rechercheSucc(courant);
+        courant->cle = asupprimer->cle;
+    }
+    if (asupprimer->gauche == NULL)
+        rattache = asupprimer->droite;
+    else
+        rattache = asupprimer->gauche;
+    if(rattache != NULL)
+        rattache->pere = asupprimer->pere;
+    if(rattache->pere == NULL) *racine = rattache;
+    else
+    {
+        if(asupprimer->pere->droite == asupprimer)
+            asupprimer->pere->droite = rattache;
+        else
+            asupprimer->pere->gauche = rattache;
+    }
+    return true;
+}
+bool supprimerRec(noeud** racine, noeud* x) {
+    if (x == NULL) return false; // rien à supprimer
+
+    // Cas 2 fils
+    if (x->gauche != NULL && x->droite != NULL) {
+        // Trouver le successeur (minimum du sous-arbre droit)
+        noeud* succ = rechercheMin(x->droite);
+        x->cle = succ->cle;                  // copier la valeur
+        return supprimerRec(racine, succ);  // supprimer le successeur récursivement
+    }
+
+    // Cas 0 ou 1 fils
+    noeud* fils;
+    if (x->gauche != NULL)
+        fils = x->gauche;
+    else
+        fils = x->droite;
+
+    // Rattacher le fils au parent
+    if (x->pere != NULL) {
+        if (x->pere->gauche == x)
+            x->pere->gauche = fils;
+        else
+            x->pere->droite = fils;
+    } else {
+        *racine = fils; // x est la racine
+    }
+
+    // Mettre à jour le parent du fils
+    if (fils != NULL)
+        fils->pere = x->pere;
+
+    // Supprimer le nœud
+    delete x;
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 // Construire un ABR à partir d’un tableau
 void createABRFromArray(noeud **racine, int tab[], int taille) {
